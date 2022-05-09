2d306bd5a6b0f838dcb07008c02cb23e88f071e8
jourmooney <jourmooney@gmail.com>
2022-05-09 21:54:08 +0900

    Add create user router

---
+++ db/db_user.py
@@ -0,0 +1,14 @@
+from sqlalchemy.orm.session import Session
+from db.models import DbUser
+from routers.schemas import UserBase
+
+def create_user(db: Session, request: UserBase):
+    new_user = DbUser(
+        username = request.username,
+        email = request.email,
+        password = request.password # we need to hash the password
+    )
+    db.add(new_user)
+    db.commit()
+    db.refresh(new_user)
+    return new_user
