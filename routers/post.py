7d08e70fbbfe9da81d1ddafc2d7b7f8011de375f
jourmooney <jourmooney@gmail.com>
2022-05-10 21:01:14 +0900

    Add get all posts method

--- routers/post.py
+++ routers/post.py
@@ -4,7 +4,7 @@ from sqlalchemy.orm import Session
 from routers.schemas import PostBase, PostDisplay
 from db.database import get_db
 from db import db_post
-
+from typing import List
 
 router = APIRouter(prefix="/post", tags=["post"])
 
@@ -19,3 +19,8 @@ def create(request: PostBase, db: Session = Depends(get_db)):
             detail="Parameter image_url_type can only take values 'absolute' or 'relative'.",
         )
     return db_post.create(db, request)
+
+
+@router.get("/all", response_model=List[PostDisplay])
+def posts(db: Session = Depends(get_db)):
+    return db_post.get_all(db)
